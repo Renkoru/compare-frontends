23ed07005124bdfee2d0ecb4a624ed90b12cb532
Maxim Rurenko <mrurenko@lineate.com>
2018-04-26 19:03:16 +0600

    add initial angular app

---
+++ mr-angular/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'hello-app',
+  styles: [require('./app.component.scss')],
+  template: require('./app.component.html'),
+})
+export class HelloComponent {}
